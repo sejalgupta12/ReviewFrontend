fc6daa662cb4655de4980fa6cdb1a25cc23e2fdd
sejalgupta12 <39177562+sejalgupta12@users.noreply.github.com>
2018-07-10 21:34:53 +0530

    updated

--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -2,18 +2,6 @@ import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
-interface myData{
-  email:string,
-  status:boolean
-}
-
-interface isLoggedIn{
-  status:boolean
-}
-
-interface logoutStatus{
-  success:boolean
-}
 
 @Injectable({
   providedIn: 'root'
@@ -23,17 +11,7 @@ export class UserService {
   constructor(private http:HttpClient) {}
 
 
-  getData(){
-    return this.http.get<myData>('/data')
-  }
 
-  isLoggedIn():Observable<isLoggedIn>{
-    return this.http.get<isLoggedIn>('/isloggedin')
-  }
-
-  logout(){
-    return this.http.get<logoutStatus>('/logout')
-  }
  
 
 }
